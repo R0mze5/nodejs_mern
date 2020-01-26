b1ffd8c3d372112b537ab1fa8ddd03491d07e4b6
Roman Khoroshilov <roman.rs.kh@gmail.com>
2020-01-26 14:33:14 +0200

    fix package.json

--- app/app.ts
+++ app/app.ts
@@ -18,9 +18,11 @@ app.use('/t', require('./routes/redirect.routes'));
 if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static(path.join(__dirname, '..', 'client', 'build')));
 
-  app.get(* , (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'client' , 'build', 'index.html'))
-  })
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '..', 'client', 'build', 'index.html'),
+    );
+  });
   // console.log(path.join(__dirname, '..', 'client', 'build'));
 }
 
